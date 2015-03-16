a9dc5e58c7a105e31fcb0fdaff75a8a7e9bd214f
Bryan Willis <willis.bryan@live.com>
2015-03-15 23:22:22 -0500

    Update bsg-grid-classes.php

---
+++ addons/bsg-grid-classes.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Clean up wp_head()
+ *
+ * Remove unnecessary <link>'s
+ * Remove inline CSS used by Recent Comments widget
+ * Remove inline CSS used by posts with galleries
+ * Remove self-closing tag and change ''s to "'s on rel_canonical()
+ *
+ * You can enable/disable this feature in functions.php):
+ * add_theme_support('bsg-grid-classes');
+ */

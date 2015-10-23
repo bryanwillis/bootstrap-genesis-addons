e81d31e5b58f4842cb94420e9fac84ca742b8a50
Bryan Willis <willis.bryan@live.com>
2015-10-23 02:41:59 -0500

    Update customizer-logo.php

--- addons/customizer-logo.php
+++ addons/customizer-logo.php
@@ -1,60 +1,46 @@
 <?php
 
-// css styling so logo doesn't overflow off the navbar
-function bsg_navbar_brand_logo_css() {
-    ?>
-<style type="text/css">
-.navbar-brand >  img {
-  object-fit: contain;
-  max-height: 100%;
-}
-</style>
-        <?php
-}
-add_action( 'wp_head', 'bsg_navbar_brand_logo_css' );
-
-// add customizer controls
-function bsg_navbar_brand_logo_customize_register( $wp_customize ) {
-$wp_customize->add_setting( 'brand_logo', 
-         array(
-            'default' => '', 
-            'type' => 'theme_mod',
-            'capability' => 'edit_theme_options',
-            'transport' => 'refresh',
-         ) 
-      );     
-$wp_customize->add_control( new WP_Customize_Image_Control(
-         $wp_customize, 
-         'bsg_brand_logo', 
-         array(
-            'label' => __( 'Navbar Logo', 'bsg' ),
-            'section' => 'title_tagline', 
-            'settings' => 'brand_logo',
-            'priority' => 10,
-         ) 
-      ) );
-}
-add_action( 'customize_register', 'bsg_navbar_brand_logo_customize_register' );
-
-// show logo if added to customizer
-function bsg_customizer_add_navbar_brand() { 
-    if( get_theme_mod( 'brand_logo') !== '' ) {
-    $output = '<a class="navbar-brand" id="logo" href="'. esc_url( home_url( '/' ) ) .'" title="'. esc_attr( get_bloginfo( 'name', 'display' ) ) .'" rel="home" ><img src="'. esc_url( get_theme_mod( 'brand_logo' ) ) .'" alt="'. esc_attr( get_bloginfo( 'name', 'display' ) ) .'"></a>';
-    } else {
-    $output .= '<a class="navbar-brand" id="logo" title="' .
-                esc_attr( get_bloginfo( 'description' ) ) .
-                '" href="' .
-                    esc_url( home_url( '/' ) ) .
-            '">';
-    $output .= get_bloginfo( 'name' );
-    $output .= '</a>'; 
+    // add customizer controls
+    add_action( 'customize_register', 'bsg_navbar_brand_logo_customize_register' );
+    function bsg_navbar_brand_logo_customize_register( $wp_customize ) {
+        $wp_customize->add_setting( 'brand_logo',
+             array(
+                'default' => '',
+                'type' => 'theme_mod',
+                'capability' => 'edit_theme_options',
+                'transport' => 'refresh',
+             )
+        );
+        $wp_customize->add_control( new WP_Customize_Image_Control(
+             $wp_customize,
+             'bsg_brand_logo',
+             array(
+                'label' => __( 'Navbar Logo', 'bsg' ),
+                'section' => 'title_tagline',
+                'settings' => 'brand_logo',
+                'priority' => 10,
+             )
+        ) );
     }
-return $output;
-}
 
-// filter default navbar
-add_filter( 'bsg_navbar_brand', 'ironcode_navbar_brand' );
-function ironcode_navbar_brand( $output ) {
-    $output = bsg_customizer_add_navbar_brand();
-    return $output;
-}
+    // show logo if it has been added via customizer
+    add_filter( 'bsg_navbar_brand', 'bsg_customizer_add_navbar_brand' );
+    function bsg_customizer_add_navbar_brand( $output ) {
+        $brand_logo = get_theme_mod( 'brand_logo' );
+        // check $brand_logo is set
+        if ( ! $brand_logo ) {
+            return $output;
+        }
+        $output = '<a class="navbar-brand" id="logo" style="padding-top: 10px; padding-bottom: 10px;" title="' .
+            esc_attr( get_bloginfo( 'description' ) ) .
+            '" href="' .
+            esc_url( home_url( '/' ) ) .
+            '">';
+        $output .= '<img style="height: 100%; max-height: 100%; width: auto; margin: 0 auto;" src="' .
+            $brand_logo .
+            '" alt="' .
+            esc_attr( get_bloginfo( 'name' ) ) .
+            '">';
+        $output .= '</a>';
+        return $output;
+    }

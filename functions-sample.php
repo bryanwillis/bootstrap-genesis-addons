fa3e11e87e7f262c311a75a96edc1cc95bdd5645
Bryan Willis <willis.bryan@live.com>
2015-10-29 03:53:57 -0500

    Update functions-sample.php

--- functions-sample.php
+++ functions-sample.php
@@ -16,3 +16,17 @@ add_theme_support('bsg-acf');
 add_theme_support('mu-plugin-autoloader');
 // Add Kirki Customizer Toolkit
 add_theme_support('kirki');
+
+
+
+
+
+/* Specific to Bootstrap Genesis */
+
+// remove_action('genesis_footer', 'genesis_do_footer');
+add_filter('genesis_footer_creds_text', 'bsg_addons_footer_creds_filter');
+function bsg_addons_footer_creds_filter( $creds ) {
+	$creds = '[footer_copyright] &middot; <a href="'. esc_url( home_url( '/' ) ) .'">'. get_bloginfo( 'name' ) .'</a>';
+	return $creds;
+}
+

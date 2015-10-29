f29f14de2a7453798cfb1b48fb6d00206fbae2d8
Bryan Willis <willis.bryan@live.com>
2015-10-28 21:34:15 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,9 @@
 <?php
 
+// To disable a feature simply comment out the added theme support below
+
+// Customizer Logo 
+add_theme_support('bsg-customizer-logo');
+
+// Customizer Widget Columns
+add_theme_support('bsg-grid-classes');

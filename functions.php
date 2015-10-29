28a3ab331f1d52a38dfa46b9e7f78b7883536801
Bryan Willis <willis.bryan@live.com>
2015-10-29 03:02:47 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,3 @@
 <?php
 
-/* To disable a feature simply comment out the added theme support below or delete it */
-/* Use filenames without .php in the bootstrap-genesis/addons/ folder as theme support varialbe/option name and prefix with bsg */ */
-/* add_theme_support ('bsg-filename-without-php-extension') */
 
-// Customizer Logo 
-add_theme_support('bsg-customizer-logo');
-
-// Customizer Widget Columns
-add_theme_support('bsg-grid-classes');
-
-// Custom CSS File
-add_theme_support('bsg-custom-css');
-
-// Add advanced-custom-fields
-add_theme_support('bsg-acf');
-
-// Add mu-plugin auto loader
-add_theme_support('mu-plugin-autoloader');
-
-// Add Kirki Customizer Toolkit
-add_theme_support('kirki');

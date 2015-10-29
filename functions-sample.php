6b8b62d1258c7feb5e04d2e757253f2f7f040f8f
Bryan Willis <willis.bryan@live.com>
2015-10-29 03:17:39 -0500

    Update and rename functions.php to functions-sample.php

---
+++ functions-sample.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Copy desired addons to bootstrap-genesis-adddons/addons/functions.php */
+/* Use filenames without .php in the bootstrap-genesis/addons/ folder as theme support varialbe/option name and prefix with bsg */ */
+/* add_theme_support ('bsg-filename-without-php-extension') */
+
+// Customizer Logo 
+add_theme_support('bsg-customizer-logo');
+// Customizer Widget Columns
+add_theme_support('bsg-grid-classes');
+// Custom CSS File
+add_theme_support('bsg-custom-css');
+// Add advanced-custom-fields
+add_theme_support('bsg-acf');
+// Add mu-plugin auto loader
+add_theme_support('mu-plugin-autoloader');
+// Add Kirki Customizer Toolkit
+add_theme_support('kirki');

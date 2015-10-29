aa77f64509daedd62f71fe8009afd1d78f505346
Bryan Willis <willis.bryan@live.com>
2015-10-28 21:54:19 -0500

    Added comments and additional Theme Support

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 
-// To disable a feature simply comment out the added theme support below
+/* To disable a feature simply comment out the added theme support below or delete it */
+/* Use filenames without .php in the bootstrap-genesis/addons/ folder as theme support varialbe/option name and prefix with bsg */ */
+/* add_theme_support ('bsg-filename-without-php-extension') */
 
 // Customizer Logo 
 add_theme_support('bsg-customizer-logo');
 
 // Customizer Widget Columns
 add_theme_support('bsg-grid-classes');
+
+// Custom CSS File
+add_theme_support('bsg-custom-css');

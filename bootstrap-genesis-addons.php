5aa3b6732a09e25a0f183ef335eaf8daae1116d6
Bryan Willis <willis.bryan@live.com>
2015-10-28 21:33:42 -0500

    Update bootstrap-genesis-addons.php

--- bootstrap-genesis-addons.php
+++ bootstrap-genesis-addons.php
@@ -21,13 +21,3 @@ function load_bsg_addons() {
   }
 }
 add_action('after_setup_theme', 'load_bsg_addons', 100);
-
-
-
-// To disable a feature simply comment out the added theme support below
-
-// Customizer Logo 
-add_theme_support('bsg-customizer-logo');
-
-// Customizer Widget Columns
-add_theme_support('bsg-grid-classes');

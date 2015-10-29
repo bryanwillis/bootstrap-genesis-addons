67b965f8ff64f057638737c448370394ef6395e9
Bryan Willis <willis.bryan@live.com>
2015-10-29 02:51:45 -0500

    Include Advanced Custom Fields

---
+++ addons/acf.php
@@ -0,0 +1,36 @@
+<?php
+
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+ 
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = plugin_dir_path( __FILE__ ) . '/addons/acf/';
+    
+    // return
+    return $path;
+    
+}
+ 
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = plugin_dir_url( __FILE__ ) . '/addons/acf/';
+    
+    // return
+    return $dir;
+    
+}
+ 
+
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( plugin_dir_url( __FILE__ ) . '/addons/acf/acf.php' );
